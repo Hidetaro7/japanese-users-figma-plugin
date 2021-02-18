08f3cbebfd1810c25f0b59376ac6eb1ce6646f33
Hidetaro7 <hidetaro7@gmail.com>
2021-02-18 14:06:32 +0900

    :feat ダミー個人情報を発行できるようにした

--- src/code.ts
+++ src/code.ts
@@ -1,92 +1,48 @@
-figma.showUI(__html__, { width: 780, height: 700 });
+import users from "./userdata"
+
+figma.showUI(__html__, { width: 400, height: 400 });
 
 figma.ui.onmessage = async msg => {
-  const newPage = figma.createPage();
-  newPage.name = "Colors";
-  let scss =
-    "// SCSS\n// --------------------------------------------------------\n\n";
+  const page:PageNode = figma.currentPage as PageNode;
+  page.selection.forEach(async anynode => {
+    if(anynode.type === "TEXT") {
+      const fontname:FontName = anynode.getRangeFontName(0, anynode.characters.length-1) as FontName;
+      await figma.loadFontAsync(fontname)
+     anynode.fontName = fontname;
 
-  if (msg.type === "create-rectangles") {
-    const nodes = [];
-    const colors = msg.colors;
-    //console.log(colors);
-    const createColorDataCode = (cd: any) => {
-      //console.log(cd);
-      return { r: cd.r / 255, g: cd.g / 255, b: cd.b / 255 };
-    };
-
-    const setColorStyle = (name: string, color: RGB) => {
-      const style = figma.createPaintStyle();
-      figma.createPaintStyle;
-      style.name = name;
-      const rgbColor: RGB = color;
-      const alpha: number = 1;
-      const solidPaint: SolidPaint = {
-        type: "SOLID",
-        color: rgbColor,
-        opacity: alpha
-      };
-      style.paints = [solidPaint];
-    };
-
-    const createSCSS = group => {
-      // scss
-      scss += `// ${group.title} \n`;
-      group.group.forEach(item => {
-        scss += `$-${group.title}-${item.label}: ${item.color}; \n`;
-      });
-      scss += "\n";
-      //console.log(scss);
-    };
-
-    colors.forEach((group, ind) => {
-      const colorGroup = group.group;
-      //console.log(group);
-      createSCSS(group);
-      for (let i = 0; i < colorGroup.length; i++) {
-        const color = createColorDataCode(colorGroup[i].rgbColor);
-        const circle = figma.createEllipse();
-        circle.name = colorGroup[i].label;
-        circle.x = i * 150 + figma.viewport.center.x;
-        circle.y = ind * 150;
-        circle.fills = [{ type: "SOLID", color: color }];
-        newPage.appendChild(circle);
-        nodes.push(circle);
-        setColorStyle("$-" + group.title + "-" + colorGroup[i].label, color);
-      }
-    });
-
-    //add scss
-    const scssText = figma.createText();
-    scssText.x = figma.viewport.center.x;
-    scssText.y = (colors.length + 1) * 150;
-    const fontlist = await figma.listAvailableFontsAsync();
-    const codeFont = fontlist.filter(font => {
-      return (
-        font.fontName.family.match(
-          /Consolas|Source Code Pro|Inconsolata|Menlo|Monaco|Roboto Mono|Roboto/gi
-        ) && font.fontName.style === "Regular"
-      );
-    });
-    if (codeFont.length !== 0) {
-      await figma.loadFontAsync({
-        family: codeFont[0].fontName.family,
-        style: "Regular"
-      });
-      scssText.fontName = codeFont[0].fontName;
+     const i:number = Math.floor(Math.random() * users.length);
+    let resUserData:string;
+    switch(msg.params) {
+      case "name" :
+        resUserData = users[i].name;
+        break;
+      case "kana" :
+        resUserData = users[i].kana;
+        break;
+      case "email" :
+        resUserData = users[i].email;
+        break;
+      case "birthday" :
+        resUserData = users[i].birthday;
+        break;
+      case "pref" :
+        resUserData = users[i].pref;
+        break;
+      case "city" :
+        resUserData = users[i].pref + users[i].city;
+        break;
+      case "town" :
+        resUserData = users[i].pref + users[i].city + users[i].town;
+        break;
+      case "add1" :
+        resUserData = users[i].pref + users[i].city + users[i].town + users[i].add1;
+        break;
     }
-    //console.log(codeFont);
-
-    scssText.fontSize = 24;
-    scssText.characters = scss;
-    newPage.appendChild(scssText);
-    nodes.push(scssText);
-    newPage.selection = nodes;
-    figma.currentPage = newPage;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+     anynode.characters = resUserData
+    }
+    
+  })
+ 
+  
+  //figma.closePlugin();
 };
--- src/ui.js
+++ src/ui.js
@@ -1,11 +1,10 @@
 import Vue from "vue";
 import App from "./App";
-import Color from "./Color";
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
-  render: h => h(Color)
+  render: h => h(App)
 });
